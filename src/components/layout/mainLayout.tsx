76988414b8939eca1ae0a375d58dd9e0153ecd38
Dave <sahajdavework@pm.me>
2024-09-01 15:50:29 +0530

    refactor: layout padding

--- src/components/layout/mainLayout.tsx
+++ src/components/layout/mainLayout.tsx
@@ -4,7 +4,7 @@ type Props = { children: JSX.Element }
 
 export const MainLayout = (props: Props) => {
   return (
-    <div className="h-screen w-screen px-6 py-8 flex flex-col gap-y-8 bg-slate-100">
+    <div className="h-screen w-screen overflow-scroll px-6 py-8 lg:px-[35%] flex flex-col gap-y-8 bg-slate-100">
       <Header />
       {props.children}
     </div>

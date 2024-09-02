77c06d4ed0e22bea1c2417b3cc757a231601e287
Dave <sahajdavework@pm.me>
2024-09-02 16:12:14 +0530

    feat: zustand store

--- src/hooks/store.tsx
+++ src/hooks/store.tsx
@@ -1,6 +1,32 @@
-import { createStore } from '@/libs/hooks'
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import { createStore, randomId } from '@/libs/hooks'
+import { storageReader, storageWriter } from '@/utils'
+
+const emptyForm = {
+  id: randomId(),
+
+  venue: null,
+  date: null,
+  participants: [],
+
+  items: [],
+  taxes: null,
+  total: null,
+  discount: null,
+} as const
 
 export const useStore = createStore((set) => ({
-  bears: 1,
-  increasePopulation: () => set((state: { bears: number }) => ({ bears: state.bears + 1 })),
+  ...emptyForm,
+  updateForm: (values: any) => set(() => ({ ...values })),
+  saveAndReset: () =>
+    set((state: any) => {
+      // eslint-disable-next-line @/no-unused-vars, @typescript-eslint/no-unused-vars
+      const { updateForm, saveAndReset, ...data } = state
+
+      const reports = storageReader('localStorage', 'reports')
+
+      if (data.venue) storageWriter('localStorage', { reports: [...(reports ? reports : []), data] })
+
+      return { ...state, ...emptyForm }
+    }),
 }))
